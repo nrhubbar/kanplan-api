d3ec1433f495ef9180468d599a7ecc6d7fe3f495
trentspi <tspice23@att.net>
2017-01-21 04:18:52 -0500

    verified user role upon task creation

--- kanplan/routes/task/tasks.js
+++ kanplan/routes/task/tasks.js
@@ -1,9 +1,10 @@
 var task = require('./task.schema.js');
 var shortid = require('shortid');
+var role = require('../role/role.schema.js');
 
 module.exports = function(app) {
   app.get('/tasks/:orgId', function(req, res){
-    task.find({'ordId':req.params.ordId, 'assignee':req.params.assignee},
+    task.find({'orgId':req.params.orgId, 'assignee':req.params.assignee},
     {
       author:true,
       title:true,
@@ -25,27 +26,33 @@ module.exports = function(app) {
     var state = (req.body.assignee == null ? "Open" : "Assigned");
     var taskId = shortid.generate();
 
-    task.create({
-      title : req.body.title,
-      description : req.body.description,
-      compensation : req.body.compensation,
-      author : req.body.author,
-      orgId : req.params.orgId,
-      assignee : req.body.assignee,
-      state : state,
-      timeWorked : 0,
-      creationTime : currentTime,
-      timeLog : {
-        startTime : null,
-        endTime : null
-      },
-      _id : taskId
-    }).then(function(task, err) {
+    role.findOne({'orgId':req.params.orgId, 'userId':req.body.author}, {'role':true}).then(role,err) {
       if (err) {
         res.status(500).send(err);
       }
-      res.json(task);
-
-    })
+      if (role.role == 'admin') {
+        task.create({
+          title : req.body.title,
+          description : req.body.description,
+          compensation : req.body.compensation,
+          author : req.body.author,
+          orgId : req.params.orgId,
+          assignee : req.body.assignee,
+          state : state,
+          timeWorked : 0,
+          creationTime : currentTime,
+          timeLog : {
+            startTime : null,
+            endTime : null
+          },
+          _id : taskId
+        }).then(function(task, err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+          res.json(task);
+        });
+      }
+    }
   });
 }
