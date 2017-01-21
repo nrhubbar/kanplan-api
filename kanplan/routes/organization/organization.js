0037c2af4cffafb7933f0753b67e60643163951f
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-01-21 08:40:16 -0500

    For the join org route I, create an entry in role and pushed orgId onto user

--- kanplan/routes/organization/organization.js
+++ kanplan/routes/organization/organization.js
@@ -1,5 +1,10 @@
 var organization = require('./organization.schema.js');
+
 var role = require('../role/role.schema.js');
+var validRoles = require('../role/validRoles.js');
+
+var user = require('../user/user.schema.js');
+
 var shortid = require('shortid');
 
 module.exports = function(app) {
@@ -33,6 +38,39 @@ module.exports = function(app) {
         res.send(organization);
       });
     });
+  });
+
+  app.post('/organization/join', function (req, res) {
+    if (req.body.role in validRoles) {
+      role.create({
+        orgId : req.body.orgId,
+        userId : req.body.userId,
+        role : req.body.role
+      }).then(function (role, err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+        user.findById(req.body.userId).then(function (user, err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+          
+          user.orgs.push(req.body.orgId);
+          user.save().then(function(){
+              res.json(user);
+
+          }, function(err){
+
+            if(err){
+              console.log(err);
+              res.status(500).send(err);
+            }
+          });
+        });
+      });
+    } else {
+      res.status(400).send("Please use valid role");
+    }
   });
 };
 
